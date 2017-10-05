47fa495d47855a250fc2efa95813c5344e07fdf3
nischal basuti <nischalbasuti1@gmail.com>
2017-10-05 14:59:23 +0530

    added demo game

---
+++ demo/settlement.js
@@ -0,0 +1,11 @@
+"use strict";
+function Settlement(x,y,width,height,world){
+    Unit.call(this,x,y,width,height,world);
+    Settlement.prototype = Object.create(Unit.prototype);
+    Settlement.prototype.constructor = Settlement;
+
+    this.sprites.push(settlementSprite);
+    this.setCurrentSprite(0);
+    this.health = 1000;
+    this.speed = 0;
+}
